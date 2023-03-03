59d8e65d182aee7c87164c1278200616d55c2883
P2720877 <P2720877@my365.dmu.ac.uk>
2023-03-03 23:24:03 +0000

    Added testing for the order class

---
+++ Testing6/tstOrder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing6
+{
+    [TestClass]
+    class tstOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Creating an instance of the sample class
+            tstOrder sampleOrder = new tstOrder();
+            //Checking if the object exist
+            Assert.IsNotNull(sampleOrder);
+        }
+
+    }
+
+ 
+}
