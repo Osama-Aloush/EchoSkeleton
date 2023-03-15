68345022b9497da2193d8d7f70a31cc02cf2598f
P2720877 <P2720877@my365.dmu.ac.uk>
2023-03-15 13:25:01 +0000

    Fixed issues for the orders unit tests

--- Testing6/tstOrder.cs
+++ Testing6/tstOrder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,76 @@ namespace Testing6
     [TestClass]
     class tstOrder
     {
+        public bool Active { get; private set; }
+        public string OrderDescription { get; private set; }
+
         [TestMethod]
         public void TestMethod1()
         {
             //Creating an instance of the sample class
-            tstOrder sampleOrder = new tstOrder();
+            clsOrder sampleOrder = new clsOrder();
             //Checking if the object exist
             Assert.IsNotNull(sampleOrder);
         }
 
+
+        [TestMethod]
+        public void OrderDescriptionPropertyOk()
+        {
+            //Creating instance of the class
+            clsOrder sampleOrder = new clsOrder();
+
+            String TestData = "Carrots";
+
+            sampleOrder.OrderDescription = TestData;
+
+            Assert.AreEqual(sampleOrder.OrderDescription,TestData);
+
+        }
+
+        [TestMethod]
+        public void OrderTotalPropertyOk()
+        {
+            clsOrder sampleOrder = new clsOrder();
+
+            int TestData = 1;
+
+            sampleOrder.OrderTotal = TestData;
+
+            Assert.AreEqual(sampleOrder.OrderTotal, TestData);
+        
+        }
+
+        [TestMethod]
+        public void HasDiscountPropertyOk()
+        {
+            clsOrder sampleOrder = new clsOrder();
+
+            bool TestData = true;
+
+            sampleOrder.HasDiscount = TestData;
+
+            Assert.AreEqual(sampleOrder.HasDiscount, TestData);
+
+        }
+
+        [TestMethod]
+        public void DiscountAmountPropertyOk()
+        {
+            clsOrder sampleOrder = new clsOrder();
+
+            int TestData = 1;
+
+            sampleOrder.DiscountAmount = TestData;
+
+            Assert.AreEqual(sampleOrder.DiscountAmount, TestData);
+
+        }
+
+
+
+
     }
 
- 
+
 }
