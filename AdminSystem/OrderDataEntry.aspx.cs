e1ea9e57cc502f3481a0c1828179e46bbe943dd1
P2720877 <P2720877@my365.dmu.ac.uk>
2023-04-19 16:04:55 +0100

    Made a presentation layer for the OrderDataEntry page

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        //Creating a new instance of a class
+        clsOrder AnOrder = new clsOrder();
+        //Capture the Order ID
+        AnOrder.Order_ID = OrderIDTextBox.Text;
+        //Storing an order into a session object
+        Session["An_Order"] = AnOrder;
+        //Redirect to the viewer page
+        Response.Redirect("OrderViewr.aspx");
     }
 }
--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //Creating instance of a class
+        clsOrder AnOrder = new clsOrder();
+        //Getting data from the session object
+        AnOrder = (clsOrder)Session["AnOrder"];
+        //Display OrderID on the page
+        Response.Redirect(AnOrder.Order_ID);
     }
 }
