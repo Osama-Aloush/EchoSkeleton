bdf3f614a456053e574097c6ed0e0fb0bcda264f
Osama Al Oush <P2630455@my365.dmu.ac.uk>
2023-04-26 21:37:37 +0100

    fixed the find button

--- AdminSystem/PaymentDataEntry.aspx.cs
+++ AdminSystem/PaymentDataEntry.aspx.cs
@@ -35,25 +35,26 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnFind_Click(object sender, EventArgs e)
     {
-        ////create an instance of the payment class
-        //clsPayment AnPayment = new clsPayment();
-        ////variable to store the primary key
-        //string NameHolder;
-        ////variable to tstore the result of hte find operation
-        //Boolean Found = false;
-        ////get the primary key entered by the user
-        //NameHolder = Convert.ToString(txtHolderName.Text);
-        ////find the record
-        //Found == AnPayment.Find(NameHolder);
-        ////if found
-        //if (Found == true)
-        //{
-        //    //display the values of the properties in the form
-        //    txtCardNumber.Text = AnPayment.CardAdded;
-        //    txtHolderName.Text = AnPayment.NameAdded;
-        //    txtExp_Date.Text = AnPayment.DateTime;
-        //    CVV.Text = AnPayment.CVVAdded;
-        //    CustomerID = AnPayment.IDAdded;
-        //    CheckBox = AnPayment.Active;
+        //create an instance of the payment class
+        clsPayment AnPayment = new clsPayment();
+        //variable to store the primary key
+        string NameHolder;
+        //variable to tstore the result of hte find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        NameHolder = Convert.ToString(txtHolderName.Text);
+        //find the record
+        Found = AnPayment.Find(NameHolder);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtCardNumber.Text = AnPayment.CardAdded;
+            txtHolderName.Text = AnPayment.NameAdded;
+            txtExp_Date.Text = AnPayment.DateTime;
+            CVV.Text = AnPayment.CVVAdded;
+            CustomerID.Text = AnPayment.IDAdded;
+            CheckBox.Checked = AnPayment.Active;
         }
     }
+}
